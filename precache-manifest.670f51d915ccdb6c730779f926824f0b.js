7e0272ad029b0c0d5ee8c8d03addfaaa6e8a370e
eriika19 <enciso.iq@gmail.com>
2019-10-06 21:58:50 -0500

    Updates

---
+++ precache-manifest.670f51d915ccdb6c730779f926824f0b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f603197b51e8b7cddd8560e8f2a7a773",
+    "url": "/MEX008-FE-Burger-Queen/index.html"
+  },
+  {
+    "revision": "b65063fc086c0c003997",
+    "url": "/MEX008-FE-Burger-Queen/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "4172bd2c440de4f9f6c6",
+    "url": "/MEX008-FE-Burger-Queen/static/css/main.cc5f9679.chunk.css"
+  },
+  {
+    "revision": "b65063fc086c0c003997",
+    "url": "/MEX008-FE-Burger-Queen/static/js/2.402047e4.chunk.js"
+  },
+  {
+    "revision": "4172bd2c440de4f9f6c6",
+    "url": "/MEX008-FE-Burger-Queen/static/js/main.6c3e6b41.chunk.js"
+  },
+  {
+    "revision": "c08034477bcddfd21ef2",
+    "url": "/MEX008-FE-Burger-Queen/static/js/runtime~main.99333e17.js"
+  },
+  {
+    "revision": "733372fdb71ccdc1485852ffc6e790b0",
+    "url": "/MEX008-FE-Burger-Queen/static/media/PB.733372fd.jpeg"
+  },
+  {
+    "revision": "afd5dee3dda904bb63815860fe6c9b72",
+    "url": "/MEX008-FE-Burger-Queen/static/media/imagebackground.afd5dee3.png"
+  },
+  {
+    "revision": "ef1c15940062969fa3caa851f42f8e93",
+    "url": "/MEX008-FE-Burger-Queen/static/media/logo.ef1c1594.png"
+  },
+  {
+    "revision": "ce32ba7be697ac44cf4f7c828a412fc1",
+    "url": "/MEX008-FE-Burger-Queen/static/media/mesereando.ce32ba7b.png"
+  },
+  {
+    "revision": "8933daf5f6f9935079b85556bee1f661",
+    "url": "/MEX008-FE-Burger-Queen/static/media/slate.8933daf5.jpeg"
+  },
+  {
+    "revision": "ed0987fe7172ac3ef9ab5df815c984e1",
+    "url": "/MEX008-FE-Burger-Queen/static/media/vbnapa.ed0987fe.jpeg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.d6e63f55a41693adb90dbbfa8659248d.js"
+  "/MEX008-FE-Burger-Queen/precache-manifest.670f51d915ccdb6c730779f926824f0b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/MEX008-FE-Burger-Queen/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
