333e101dc39fc9a85a0163b1252a3c25d95bab35
VianeyLaraRangel <vianey.lara.rangel@gmail.com>
2019-10-10 00:42:47 -0500

    Creando mapa de rutas en Main.js dentro de Components, importando en App.js  y modificando el Index.js agregando etiqueta BrowserRouter que contiene App.js

---
+++ burguerqueen/src/pages/Main.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import GetIn from './GetIn';
+import Home from './Home';
+import Menu from './Menu';
+// import Kitchen from './Kitchen'
+
+const Main = () => (
+    <Switch>
+        <Route exact path="/" component={GetIn} />
+        {/* <Route exact path="/getin" component={GetIn} /> */}
+        <Route exact path="/home" component={Home} />
+        <Route exact path="/menu" component={Menu} />
+        {/* <Route exact path="/kitchen" component={Kitchen} /> */}
+    </Switch>
+)
+
+export default Main;
